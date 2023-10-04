163ed31c9475e299548e389e05ae07a2a3d73953
w3schools-test <zhou.yubin.joe@gmail.com>
2023-10-04 17:22:14 +0800

    update

--- src/component/Home.jsx
+++ src/component/Home.jsx
@@ -113,10 +113,10 @@ export function Home() {
 
           <p style={pStyle}>
             <Link
-              href="https://sap.github.io/ui5-webcomponents-react/?path=/docs/getting-started--docs"
+              href="https://github.com/Joe-Zhou-Yubin/MyPortfolio"
               target="_blank"
             >
-              Website Built using SAP UI5 Documentation
+              Website Built using SAP UI5 Documentation (Github Repo)
             </Link>
           </p>
         </FlexBox>
